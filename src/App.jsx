27db6daac89085dd6528981fe3fbe63a0af8692d
llipe_20 <silvahlipe@gmail.com>
2024-05-15 19:37:30 -0300

    Dependências de projeto

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="bg-red-500">
+          Bom dia
+      </header>
+    </div>
+  );
+}
+
+export default App;
